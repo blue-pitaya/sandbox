ad357d695e501ba681cb03a389cf28e67fd6a098
blue-pitaya <blue.pitaya@pm.me>
2022-06-20 13:29:06 +0200

    some future mappings

---
+++ src/main/scala/example/codestyle/FuturePath.scala
@@ -0,0 +1,20 @@
+package example.codestyle
+
+import scala.concurrent.Future
+import akka.Done
+import scala.concurrent._
+import ExecutionContext.Implicits.global
+
+object FuturePath {
+  /*
+   *  Fut[Boolean] => Success(v) =(v)> true  => Fut[Done]   => Success
+   *                  Failure(e)       false => CustomError    Failure(e)
+   */
+  sealed trait CustomError
+  final case object SomeCustomError extends CustomError
+
+  def foo(v1: Boolean, fut2: Future[Done]): Future[Either[CustomError, Done]] = {
+    val fut1 = Future(v1)
+    fut1.flatMap(v => if (v) fut2.map(Right(_)) else Future(Left(SomeCustomError)))
+  }
+}
